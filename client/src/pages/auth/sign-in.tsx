145ab6cb6cfef5a853b5259d7dcdbcddc5a250db
Ztrohub <ztroidmail@gmail.com>
2025-10-20 22:00:08 +0700

    feat(auth): update branding and localization in sign-in page

--- client/src/pages/auth/sign-in.tsx
+++ client/src/pages/auth/sign-in.tsx
@@ -20,7 +20,7 @@ export default function SignIn() {
             >
               <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
             </svg>
-            Shadcn Admin
+            Asia Teknik Service
           </div>
 
           <img
@@ -31,7 +31,7 @@ export default function SignIn() {
             alt='Vite'
           />
 
-          <div className='relative z-20 mt-auto'>
+          {/* <div className='relative z-20 mt-auto'>
             <blockquote className='space-y-2'>
               <p className='text-lg'>
                 &ldquo;This library has saved me countless hours of work and
@@ -40,34 +40,20 @@ export default function SignIn() {
               </p>
               <footer className='text-sm'>Sofia Davis</footer>
             </blockquote>
-          </div>
+          </div> */}
         </div>
         <div className='lg:p-8'>
           <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[350px]'>
             <div className='flex flex-col space-y-2 text-left'>
               <h1 className='text-2xl font-semibold tracking-tight'>Login</h1>
               <p className='text-sm text-muted-foreground'>
-                Enter your email and password below <br />
-                to log into your account
+                Masukkan user dan password anda <br />
+                untuk melakukan login
               </p>
             </div>
             <UserAuthForm />
             <p className='px-8 text-center text-sm text-muted-foreground'>
-              By clicking login, you agree to our{' '}
-              <a
-                href='/terms'
-                className='underline underline-offset-4 hover:text-primary'
-              >
-                Terms of Service
-              </a>{' '}
-              and{' '}
-              <a
-                href='/privacy'
-                className='underline underline-offset-4 hover:text-primary'
-              >
-                Privacy Policy
-              </a>
-              .
+              Dibuat sepenuh hati oleh Lukas Budi.
             </p>
           </div>
         </div>
